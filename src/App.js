c604729a5b5842f646be48dc84cb93af4932c78a
NiceTwice <fisun.serge76@gmail.com>
2018-03-28 19:18:50 +0000

    first

---
+++ src/App.js
@@ -0,0 +1,38 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+
+    }
+  }
+  componentDidMount(){
+    this.wavesurfer = window.WaveSurfer.create({
+      container: '#waveform',
+      waveColor: 'violet',
+      progressColor: 'purple',
+      scrollParent: false,
+      minPxPerSec: 200,
+      height: 300,
+//      normalize: true,
+      barHeight: .3
+    });
+    this.wavesurfer.load('/audio1.mp3');
+  }
+  play = () => {
+    this.wavesurfer.playPause();
+  };
+  render() {
+    return (
+      <div className="App">
+        <div id="waveform" style={{width:'100%', height:'300px'}}></div>
+        <button onClick={this.play}>play</button>
+      </div>
+    );
+  }
+}
+
+export default App;
