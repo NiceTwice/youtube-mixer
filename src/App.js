c5af113845c5f1b25959c3e8b3586df6702edc83
NiceTwice <fisun.serge76@gmail.com>
2018-06-07 12:34:44 +0000

    add scss support

--- src/App.js
+++ src/App.js
@@ -1,6 +1,4 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
 
 class App extends Component {
   constructor(props){
@@ -16,9 +14,9 @@ class App extends Component {
       progressColor: '#ea5300',
       scrollParent: false,
       minPxPerSec: 200,
-      height: 300,
+      height: 100,
 //      normalize: true,
-      barHeight: .3,
+      barHeight: .8,
       barWidth: 2
     });
     this.wavesurfer.load('/assets/audio1.mp3');
@@ -29,7 +27,7 @@ class App extends Component {
   render() {
     return (
         <div className="App">
-          <div id="waveform" style={{width:'100%', height:'300px'}}></div>
+          <div id="waveform" style={{width:'100%', height:'100px'}}></div>
           <button onClick={this.play}>play</button>
         </div>
     );
